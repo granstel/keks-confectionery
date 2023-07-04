09f79fa64c224775ed06abee2700040176995585
Stepan Grankin <sgrankin@inbox.ru>
2023-07-04 17:30:05 +0300

    backLink

---
+++ project/src/components/back-link/back-link.tsx
@@ -0,0 +1,12 @@
+export default function backLink():JSX.Element {
+  return (
+    <div className="back-link">
+      <div className="container"><a className="back-link__link" href="#">Назад
+        <svg className="back-link__icon" width="30" height="16" aria-hidden="true">
+          <use xlinkHref="#icon-arrow-left"></use>
+        </svg>
+      </a>
+      </div>
+    </div>
+  );
+}
