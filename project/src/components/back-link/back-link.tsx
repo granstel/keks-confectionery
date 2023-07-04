6d349739a237b043aa6fb950afde72875011b4ed
Stepan Grankin <sgrankin@inbox.ru>
2023-07-04 17:39:59 +0300

    fixed href

--- project/src/components/back-link/back-link.tsx
+++ project/src/components/back-link/back-link.tsx
@@ -2,7 +2,7 @@ export default function BackLink():JSX.Element {
   return (
     <div className="back-link">
       <div className="container">
-        <a className="back-link__link" href="#">Назад
+        <a className="back-link__link" href="/">Назад
           <svg className="back-link__icon" width="30" height="16" aria-hidden="true">
             <use xlinkHref="#icon-arrow-left"></use>
           </svg>
