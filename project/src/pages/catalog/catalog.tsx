ae6ec7f8e255cee108ab9a75718c8e69126878a7
Stepan Grankin <sgrankin@inbox.ru>
2023-07-05 21:24:54 +0300

    Catalog

---
+++ project/src/pages/catalog/catalog.tsx
@@ -0,0 +1,67 @@
+import BackLink from '../../components/back-link/back-link';
+import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
+import ProductCard from '../../components/product/product-card';
+
+export default function Catalog(): JSX.Element {
+  return (
+    <>
+      <Header />
+      <BackLink />
+      <main>
+        <div className="catalog-filter">
+          <div className="container">
+            <div className="catalog-filter__first-level">
+              <h3 className="catalog-filter__title catalog-filter__title--first-level">основы</h3>
+              <ul className="catalog-filter__list catalog-filter__list--first-level">
+                <li className="catalog-filter__item catalog-filter__item--first-level">
+                  <button className="btn btn--filter-first-level" type="button">Бисквит</button>
+                </li>
+                <li className="catalog-filter__item catalog-filter__item--first-level">
+                  <button className="btn btn--filter-first-level" type="button">Десерт</button>
+                </li>
+                <li className="catalog-filter__item catalog-filter__item--first-level">
+                  <button className="btn btn--filter-first-level" type="button">Чизкейк</button>
+                </li>
+                <li className="catalog-filter__item catalog-filter__item--first-level">
+                  <button className="btn btn--filter-first-level" type="button">Песочное</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <section className="catalog">
+          <div className="container">
+            <h2 className="visually-hidden">Каталог</h2>
+            <div className="catalog__wrapper">
+              <ul className="catalog__list">
+                <li className="catalog__item">
+                  <ProductCard />
+                </li>
+                <li className="catalog__item">
+                  <ProductCard />
+                </li>
+                <li className="catalog__item">
+                  <ProductCard />
+                </li>
+                <li className="catalog__item">
+                  <ProductCard />
+                </li>
+                <li className="catalog__item">
+                  <ProductCard />
+                </li>
+                <li className="catalog__item">
+                  <ProductCard />
+                </li>
+              </ul>
+              <div className="catalog__button-wrapper">
+                <button className="btn btn--second" type="button">Показать еще</button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
