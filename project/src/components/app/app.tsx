2c817b3b8cdd0468b5862ba90192ef0b3a3d363e
Stepan Grankin <sgrankin@inbox.ru>
2023-07-05 09:24:21 +0300

    Routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,16 @@
 import Main from '../../pages/main/main';
+import {Route, Routes} from "react-router-dom";
+import {AppRoute} from "../../const";
+import Product from "../../pages/Product/Product";
 
 function App(): JSX.Element {
-  return <Main />;
+  return (
+    <Routes>
+      <Route path={AppRoute.Main} element={<Main />}/>
+      <Route path={AppRoute.Product} element={<Product />}/>
+    </Routes>
+  );
+
 }
 
 export default App;
