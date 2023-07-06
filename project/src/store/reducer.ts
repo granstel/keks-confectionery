422580f8e6b1c95e4fe87ce006b23d9085d4fee1
Stepan Grankin <sgrankin@inbox.ru>
2023-07-06 17:40:07 +0300

    action at reducer

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,13 +1,17 @@
-﻿import {createReducer} from "@reduxjs/toolkit";
+import {createReducer} from '@reduxjs/toolkit';
+import {productsLoaded} from './action';
 
 type State = {
-
+  products: undefined;
 }
 
 const initialState: State = {
-
+  products: undefined
 };
 
 export const reducer = createReducer(initialState, (builder) => {
-
+  builder
+    .addCase(productsLoaded, (state, action) => {
+      state.products = action.payload;
+    });
 });
