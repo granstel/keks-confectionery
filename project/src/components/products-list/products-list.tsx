d0a89dcf49231355a3327dabdc2e3f135f35fc01
Stepan Grankin <sgrankin@inbox.ru>
2023-07-06 23:44:18 +0300

    trying to use children

--- project/src/components/products-list/products-list.tsx
+++ project/src/components/products-list/products-list.tsx
@@ -1,31 +1,24 @@
 import ProductCard from '../product/product-card';
-import {Link} from 'react-router-dom';
-import {AppRoute} from '../../const';
 
-export default function ProductsList():JSX.Element {
+type productsListProps = {
+  classNamePrefix: string,
+  children: JSX.Element | undefined;
+}
+
+export default function ProductsList(props: productsListProps):JSX.Element {
+  const {classNamePrefix, children} = props;
   return (
-    <ul className="random-main__list">
-      <li className="random-main__item">
+    <ul className={`${classNamePrefix}__list`}>
+      <li className={`${classNamePrefix}__item`}>
         <ProductCard />
       </li>
-      <li className="random-main__item">
+      <li className={`${classNamePrefix}__item`}>
         <ProductCard />
       </li>
-      <li className="random-main__item">
+      <li className={`${classNamePrefix}__item`}>
         <ProductCard />
       </li>
-      <li className="random-main__item">
-        <Link className="random-main__link" to={AppRoute.Catalog}>
-          <div className="random-main__icon-wrapper">
-            <div className="random-main__icon">
-              <svg width="120" height="130" aria-hidden="true">
-                <use xlinkHref="#icon-keks"></use>
-              </svg>
-            </div>
-          </div>
-          <h3 className="random-main__subtitle">Все кексы</h3>
-        </Link>
-      </li>
+      {children ?? ''}
     </ul>
   );
 }
--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -29,7 +29,20 @@ export default function Main(): JSX.Element {
         <section className="random-main">
           <div className="container">
             <h2 className="random-main__title">кексы</h2>
-            <ProductsList />
+            <ProductsList>
+              <li className="random-main__item">
+                <Link className="random-main__link" to={AppRoute.Catalog}>
+                  <div className="random-main__icon-wrapper">
+                    <div className="random-main__icon">
+                      <svg width="120" height="130" aria-hidden="true">
+                        <use xlinkHref="#icon-keks"></use>
+                      </svg>
+                    </div>
+                  </div>
+                  <h3 className="random-main__subtitle">Все кексы</h3>
+                </Link>
+              </li>
+            </ProductsList>
           </div>
         </section>
         <section className="last-review">
