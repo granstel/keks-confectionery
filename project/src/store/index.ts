25b752e5a99f6253d4623f4920f9b3063f65dac4
Stepan Grankin <sgrankin@inbox.ru>
2023-07-06 17:30:45 +0300

    store index

---
+++ project/src/store/index.ts
@@ -0,0 +1,15 @@
+﻿import {createAPI} from "../services/api";
+import {configureStore} from "@reduxjs/toolkit";
+import {reducer} from "./reducer";
+
+const api = createAPI();
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api
+      }
+    })
+});
