c1a1c84eed608ddcdd1bec258cd7e30e2e243437
Stepan Grankin <sgrankin@inbox.ru>
2023-07-06 09:34:43 +0300

    useScrollToTop

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,8 +5,11 @@ import Product from '../../pages/Product/Product';
 import Register from '../../pages/register/register';
 import Login from '../../pages/login/login';
 import Catalog from '../../pages/catalog/catalog';
+import {useScrollToTop} from '../../hooks/use-scroll-to-top';
 
 function App(): JSX.Element {
+  useScrollToTop();
+
   return (
     <Routes>
       <Route path={AppRoute.Main} element={<Main />}/>
