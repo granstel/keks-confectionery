e1d352b41bd61552be3bff4a4b10ef1485c4608f
Stepan Grankin <sgrankin@inbox.ru>
2023-07-06 17:22:30 +0300

    api

---
+++ project/src/services/api.ts
@@ -0,0 +1,11 @@
+﻿import axios, {AxiosInstance} from "axios";
+
+const BACKEND_URL = 'https://grading.design.pages.academy/v0/keks/';
+const REQUEST_TIMEOUT = 5000;
+
+export const createAPI = (): AxiosInstance => {
+  return axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT
+  });
+};
