f3944e91c5182f539a7cd2d93ab756fce291e566
Stepan Grankin <sgrankin@inbox.ru>
2023-07-07 21:53:16 +0300

    Products parameter at productsLoaded

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,3 +1,4 @@
 import {createAction} from '@reduxjs/toolkit';
+import { Products } from '../types/product';
 
-export const productsLoaded = createAction<undefined>('productsLoaded');
+export const productsLoaded = createAction<Products>('productsLoaded');
