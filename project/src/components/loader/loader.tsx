63c0824274147c7ef03d412780ec561ead9d94f9
Stepan Grankin <sgrankin@inbox.ru>
2023-07-07 22:54:50 +0300

    show loader or child

--- project/src/components/loader/loader.tsx
+++ project/src/components/loader/loader.tsx
@@ -1,7 +1,18 @@
-export default function Loader(): JSX.Element {
-  return (
-    <div className="container" style={{ justifyContent: 'center', display: 'inline-flex' }}>
-      <div className="loader"></div>
-    </div>
-  );
+type loaderProps = {
+  isShow?: boolean;
+}
+
+export default function Loader({ children, isShow = false }: React.PropsWithChildren<loaderProps>): JSX.Element {
+
+  if (isShow) {
+    return (
+      <>
+        {!!isShow &&
+          <div className="container" style={{ justifyContent: 'center', display: 'inline-flex' }}>
+            <div className="loader"></div>
+          </div>}
+        {isShow && children}
+      </>
+    );
+  }
 }
