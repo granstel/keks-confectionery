388d7721cd2b2edb849eb3afa53c20d4ab555eff
Stepan Grankin <sgrankin@inbox.ru>
2023-07-07 09:32:07 +0300

    optional showMore prop

--- project/src/components/products-list/products-list.tsx
+++ project/src/components/products-list/products-list.tsx
@@ -1,23 +1,30 @@
 import ProductCard from '../product/product-card';
 
 type productsListProps = {
-  classNamePrefix: string,
+  classNamePrefix: string;
+  showMore?: boolean;
 }
 
 export default function ProductsList(props: React.PropsWithChildren<productsListProps>):JSX.Element {
-  const {classNamePrefix, children} = props;
+  const { classNamePrefix, children, showMore = false } = props;
   return (
-    <ul className={`${classNamePrefix}__list`}>
-      <li className={`${classNamePrefix}__item`}>
-        <ProductCard />
-      </li>
-      <li className={`${classNamePrefix}__item`}>
-        <ProductCard />
-      </li>
-      <li className={`${classNamePrefix}__item`}>
-        <ProductCard />
-      </li>
-      {children}
-    </ul>
+    <>
+      <ul className={`${classNamePrefix}__list`}>
+        <li className={`${classNamePrefix}__item`}>
+          <ProductCard />
+        </li>
+        <li className={`${classNamePrefix}__item`}>
+          <ProductCard />
+        </li>
+        <li className={`${classNamePrefix}__item`}>
+          <ProductCard />
+        </li>
+        {children}
+      </ul>
+      {showMore &&
+        <div className="catalog__button-wrapper">
+          <button className="btn btn--second" type="button">Показать еще</button>
+        </div>}
+    </>
   );
 }
