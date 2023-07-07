e4e8d268b7343676697faadd89b9b9e70b80f5fa
Stepan Grankin <sgrankin@inbox.ru>
2023-07-07 21:53:39 +0300

    fixes by linter

--- project/src/types/product.ts
+++ project/src/types/product.ts
@@ -1,13 +1,13 @@
 export type Product = {
-  id: string,
-  title: string,
-  category: string,
-  type: string,
-  price: number,
-  previewImage: string,
-  previewImageWebp: string,
-  isFavorite: boolean,
-  isNew: boolean
+  id: string;
+  title: string;
+  category: string;
+  type: string;
+  price: number;
+  previewImage: string;
+  previewImageWebp: string;
+  isFavorite: boolean;
+  isNew: boolean;
 };
 
 export type Products = Product[];
