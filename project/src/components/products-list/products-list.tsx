20f349cf245d8e0af212a2bbb98252461308287d
Stepan Grankin <sgrankin@inbox.ru>
2023-07-07 22:57:10 +0300

    Loader at ProductsList

--- project/src/components/products-list/products-list.tsx
+++ project/src/components/products-list/products-list.tsx
@@ -1,5 +1,7 @@
+import { useAppSelector } from '../../hooks';
 import { store } from '../../store';
 import { loadProducts } from '../../store/api-actions';
+import Loader from '../loader/loader';
 import ProductCard from '../product/product-card';
 
 type productsListProps = {
@@ -12,8 +14,11 @@ store.dispatch(loadProducts());
 
 export default function ProductsList(props: React.PropsWithChildren<productsListProps>):JSX.Element {
   const { classNamePrefix, children, bigCards, showMore = false } = props;
+
+  const isProductsLoading = useAppSelector((state) => state.isProductsLoading);
+
   return (
-    <>
+    <Loader isShow={ isProductsLoading }>
       <ul className={`${classNamePrefix}__list`}>
         <li className={`${classNamePrefix}__item`}>
           <ProductCard bigCards={bigCards} />
@@ -27,9 +32,9 @@ export default function ProductsList(props: React.PropsWithChildren<productsList
         {children}
       </ul>
       {showMore &&
-        <div className="catalog__button-wrapper">
-          <button className="btn btn--second" type="button">Показать еще</button>
-        </div>}
-    </>
+          <div className="catalog__button-wrapper">
+            <button className="btn btn--second" type="button">Показать еще</button>
+          </div>}
+    </Loader>
   );
 }
