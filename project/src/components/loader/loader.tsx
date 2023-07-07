82af10da8e376ffe716a5f4fc604690d05da3116
Stepan Grankin <sgrankin@inbox.ru>
2023-07-07 22:28:30 +0300

    loader

---
+++ project/src/components/loader/loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader(): JSX.Element {
+  return (
+    <div className="container" style={{ justifyContent: 'center', display: 'inline-flex' }}>
+      <div className="loader"></div>
+    </div>
+  );
+}
