9685d795f6bdc6315f66f6a2ee6b61f6e12ac7ce
Stepan Grankin <sgrankin@inbox.ru>
2023-07-09 16:08:46 +0300

    productLoading and productLoaded actions reducer

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,23 +1,34 @@
 import {createReducer} from '@reduxjs/toolkit';
-import { Products } from '../types/product';
-import {productsLoaded, productsLoading} from './action';
+import { Product, Products } from '../types/product';
+import {productLoaded, productLoading, productsLoaded, productsLoading} from './action';
 
 type State = {
   products: Products;
   isProductsLoading: boolean;
+  product?: Product;
+  isProductLoading: boolean;
 }
 
 const initialState: State = {
   products: [],
-  isProductsLoading: false
+  isProductsLoading: false,
+  product: undefined,
+  isProductLoading: false
 };
 
 export const reducer = createReducer(initialState, (builder) => {
   builder
+    .addCase(productsLoading, (state, action) => {
+      state.isProductsLoading = action.payload;
+    })
     .addCase(productsLoaded, (state, action) => {
       state.products = action.payload;
     })
-    .addCase(productsLoading, (state, action) => {
-      state.isProductsLoading = action.payload;
-    });
+    .addCase(productLoading, (state, action) => {
+      state.isProductLoading = action.payload;
+    })
+    .addCase(productLoaded, (state, action) => {
+      state.product = action.payload;
+    })
+    ;
 });
