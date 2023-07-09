06a4181d953f7af0ce4cddd2d1ecc57407d2c81e
Stepan Grankin <sgrankin@inbox.ru>
2023-07-09 09:58:35 +0300

    used product propertuy

--- project/src/components/product/product-card.tsx
+++ project/src/components/product/product-card.tsx
@@ -1,32 +1,37 @@
-import React from 'react';
 import {generatePath, Link} from 'react-router-dom';
 import {AppRoute} from '../../const';
+import { Product } from '../../types/product';
 
 type ProductCardProps = {
-  bigCards?: boolean;
+  bigCard?: boolean;
+  product: Product;
 }
 
 export default function ProductCard(props: ProductCardProps): JSX.Element {
-  const { bigCards } = props;
+  const { bigCard, product } = props;
   return (
-    <div className={`card-item ${bigCards === true ? 'card-item--big' : ''}`}>
-      <Link className="card-item__img-link" to={generatePath(AppRoute.Product, {id : '1'})}>
+    <div className={`card-item ${bigCard === true ? 'card-item--big' : ''}`}>
+      <Link className="card-item__img-link" to={generatePath(AppRoute.Product, { id: product.id })}>
         <div className="card-item__img-wrapper">
           <picture>
-            <source type="image/webp" srcSet="/img/content/blueberry-cake.webp, /img/content/blueberry-cake@2x.webp 2x" />
-            <img src="/img/content/blueberry-cake.jpg" srcSet="/img/content/blueberry-cake@2x.jpg 2x" width="241" height="245" alt="Торт голубика." />
+            <source type="image/webp" srcSet={product.previewImageWebp} />
+            <img src={product.previewImage} width="241" height="245" alt={product.title} />
           </picture>
-        </div><span className="card-item__label">Новинка</span>
+        </div>
+        {product.isNew && <span className="card-item__label">Новинка</span>}
       </Link>
-      <button className="card-item__favorites card-item__favorites--active"><span className="visually-hidden">Добавить в избранное</span>
+      <button className={`card-item__favorites ${product.isFavorite ? 'card-item__favorites--active' : ''}`}>
+        <span className="visually-hidden">Добавить в избранное</span>
         <svg width="51" height="41" aria-hidden="true">
           <use xlinkHref="#icon-like"></use>
         </svg>
       </button>
-      {bigCards && <span className="card-item__price">9 300 p</span>}
-      <a className="card-item__link" href="/1">
-        <h3 className="card-item__title"><span>Торт Голубика</span></h3>
-      </a>
+      {bigCard && <span className="card-item__price">{product.price} p</span>}
+      <Link className="card-item__link" to={generatePath(AppRoute.Product, { id: product.id })}>
+        <h3 className="card-item__title">
+          <span>{product.title}</span>
+        </h3>
+      </Link>
     </div>
   );
 }
