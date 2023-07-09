1f55bc6d4a41eb48cf57d897656ff1dc5d866284
Stepan Grankin <sgrankin@inbox.ru>
2023-07-09 16:07:34 +0300

    productLoading, productLoaded actions

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -3,3 +3,5 @@ import { Products } from '../types/product';
 
 export const productsLoaded = createAction<Products>('productsLoaded');
 export const productsLoading = createAction<boolean>('productsLoading');
+export const productLoaded = createAction<Product>('productLoaded');
+export const productLoading = createAction<boolean>('productLoading');
