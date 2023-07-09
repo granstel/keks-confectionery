fa5fd05e6a3ba3815147a50b136b0b83506a3ecb
Stepan Grankin <sgrankin@inbox.ru>
2023-07-09 16:35:31 +0300

    load product and use it

--- project/src/pages/Product/Product.tsx
+++ project/src/pages/Product/Product.tsx
@@ -3,8 +3,46 @@ import ReviewsList from '../../components/reviews-list/reviews-list';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import BackLink from '../../components/back-link/back-link';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { store } from '../../store';
+import { loadProduct } from '../../store/api-actions';
+import { useAppSelector } from '../../hooks';
 
 export default function Product(): JSX.Element {
+  const { id } = useParams();
+
+  const isProductLoading = useAppSelector((state) => state.isProductLoading);
+  const product = useAppSelector((state) => state.product);
+
+  useEffect(() => {
+    if (isProductLoading) {
+      return;
+    }
+
+    if (!product && id) {
+      store.dispatch(loadProduct(id));
+    }
+  }, [isProductLoading, id, product]);
+
+  function showStars(rating: number | undefined): JSX.Element[] {
+    const result: JSX.Element[] = [];
+
+    if (!rating) {
+      return result;
+    }
+
+    for (let i = 0; i < rating; i++) {
+      result.push(
+        <svg className="star-rating__star star-rating__star--active" width="30" height="30" aria-hidden="true">
+          <use xlinkHref="#icon-star"></use>
+        </svg>
+      );
+    }
+    return result;
+  }
+
+
   return (
     <>
       <Header />
@@ -13,35 +51,27 @@ export default function Product(): JSX.Element {
         <div className="container">
           <div className="item-details__wrapper">
             <div className="item-details__top-wrapper">
-              <h2 className="item-details__name">Чизкейк Лимонный</h2><span className="item-details__price">4 100 р</span>
+              <h2 className="item-details__name">{product?.title}</h2>
+              <span className="item-details__price">{product?.price} р</span>
             </div>
-            <div className="item-details__weight-wrapper"><span className="item-details__weight">1 300 грамм</span></div>
+            <div className="item-details__weight-wrapper">
+              <span className="item-details__weight">{product?.weight} грамм</span>
+            </div>
             <div className="item-details__bottom-wrapper">
               <div className="item-details__image-wrapper">
                 <picture>
-                  <source type="image/webp" srcSet="/img/content/lemon-pie.webp, /img/content/lemon-pie@2x.webp 2x" />
-                  <img src="/img/content/lemon-pie.jpg" srcSet="/img/content/lemon-pie@2x.jpg 2x" width="241" height="245" alt="Чизкейк лимонный" />
-                </picture><span className="item-details__label">Новинка</span>
+                  <source type="image/webp" srcSet={product?.previewImageWebp} />
+                  <img src={product?.previewImage} width="241" height="245" alt={product?.title} />
+                </picture>
+                {product?.isNew && <span className="item-details__label">Новинка</span>}
               </div>
               <div className="item-details__review-wrapper">
                 <div className="star-rating star-rating--big">
-                  <svg className="star-rating__star star-rating__star--active" width="30" height="30" aria-hidden="true">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                  <svg className="star-rating__star star-rating__star--active" width="30" height="30" aria-hidden="true">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                  <svg className="star-rating__star star-rating__star--active" width="30" height="30" aria-hidden="true">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                  <svg className="star-rating__star star-rating__star--active" width="30" height="30" aria-hidden="true">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                  <svg className="star-rating__star star-rating__star--active" width="30" height="30" aria-hidden="true">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg><span className="star-rating__count">26</span>
+                  {showStars(product?.rating)}
+                  <span className="star-rating__count">{product?.reviewCount}</span>
                 </div>
-                <div className="item-details__text-wrapper"><span className="item-details__text">Цитрусовый десерт с тонким сливочным вкусом, лёгкой свежестью и низким содержанием калорий сд</span>
+                <div className="item-details__text-wrapper">
+                  <span className="item-details__text">{product?.description?.slice(0, 140)}</span>
                   <button className="item-details__more"><span className="visually-hidden">Читать полностью</span>
                     <svg width="27" height="17" aria-hidden="true">
                       <use xlinkHref="#icon-more"></use>
