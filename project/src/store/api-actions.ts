d7b59ed21e18b5a616db93b263ac79683732b8c2
Stepan Grankin <sgrankin@inbox.ru>
2023-07-09 16:09:14 +0300

    productLoading and productLoaded api actions

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosInstance } from 'axios';
 import { APIRoute } from '../const';
-import { Products } from '../types/product';
+import { Product, Products } from '../types/product';
 import { AppDispatch, State } from '../types/state';
-import { productsLoaded, productsLoading } from './action';
+import { productLoaded, productLoading, productsLoaded, productsLoading } from './action';
 
 type payload = {
   dispatch: AppDispatch;
@@ -20,5 +20,17 @@ export const loadProducts = createAsyncThunk<void, undefined, payload>(
     dispatch(productsLoaded(data));
 
     dispatch(productsLoading(false));
+  }
+);
+
+export const loadProduct = createAsyncThunk<void, string, payload>(
+  'data/loadProduct',
+  async (productId, { dispatch, extra: api }) => {
+    dispatch(productLoading(true));
+
+    const { data } = await api.get<Product>(`${APIRoute.Products}/${productId}`);
+    dispatch(productLoaded(data));
+
+    dispatch(productLoading(false));
   }
 );
